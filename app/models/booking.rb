d237c4abbc003dda1a72243757e484c472c1df06
Kirk Patrick <kirkgo@gmail.com>
2022-06-10 08:55:29 +0100

    Added status enum to Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,6 +7,8 @@ class Booking < ApplicationRecord
   validates :passenger_id, presence: true
   validate :one_per_trip_and_passenger
 
+  enum status: %i[new confirmed cancelled]
+
   def one_per_trip_and_passenger
     if Booking.where(passenger_id: passenger_id, trip_id: trip_id).exists?
       errors.add(:passenger_id, "with name #{ Passenger.find(passenger_id).name } already booked on this trip")
